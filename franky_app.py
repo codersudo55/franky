bd80910d2475d9aad5b2d89dbe8533f38e1a42ca
Sudarshan <yes.sudarshan98@gmail.com>
2026-04-20 09:04:10 +0530

    feat: adding logo and header UI

--- franky_app.py
+++ franky_app.py
@@ -1,19 +1,40 @@
-# main.py
+# franky_app.py
 import sys
+import os
 from PySide6.QtWidgets import (
     QApplication, QWidget, QVBoxLayout,
-    QLineEdit, QPushButton, QTextEdit, QMessageBox
+    QLineEdit, QPushButton, QTextEdit, QMessageBox,
+    QLabel,QHBoxLayout
 )
+from PySide6.QtGui import QIcon, QPixmap
+from PySide6.QtCore import Qt
+
 from core.file_loader import load_file
 
 class FrankyApp(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Franky")
+        self.setWindowIcon(QIcon(os.path.join("assets", "franky_logo.png")))
         self.setGeometry(500, 300, 600, 400)
 
-        layout = QVBoxLayout()
+        main_layout = QVBoxLayout()
 
+        # header layout (logo and title)
+        header_layout = QHBoxLayout()
+
+        logo_label = QLabel()
+        pixMap = QPixmap(os.path.join("assets","franky_logo.png"))
+        logo_label.setPixmap(pixMap.scaled(50, 50, Qt.KeepAspectRatio, Qt.SmoothTransformation))
+
+        title_label = QLabel("Franky")
+        title_label.setStyleSheet("font-size: 18px; font-weight: bold;")
+
+        header_layout.addWidget(logo_label)
+        header_layout.addWidget(title_label)
+        header_layout.addStretch()
+
+        # input + button + output
         self.input_box = QLineEdit()
         self.input_box.setPlaceholderText("Enter file path...")
 
@@ -22,12 +43,15 @@ class FrankyApp(QWidget):
         self.output = QTextEdit()
         self.output.setReadOnly(True)
 
-        layout.addWidget(self.input_box)
-        layout.addWidget(self.button)
-        layout.addWidget(self.output)
+        # adding everything to main layout
+        main_layout.addLayout(header_layout)
+        main_layout.addWidget(self.input_box)
+        main_layout.addWidget(self.button)
+        main_layout.addWidget(self.output)
 
-        self.setLayout(layout)
+        self.setLayout(main_layout)
 
+        # connect button
         self.button.clicked.connect(self.load_file)
 
     def load_file(self):
